d77450bce81594d20c74446aa3b9a390dfce1408
Maxime Mouchet <max@maxmouchet.com>
2020-07-19 17:38:59 +0200

    Add cache

---
+++ test/test_cache.py
@@ -0,0 +1,17 @@
+from datetime import timedelta
+
+from mtoolbox.cache import Cache
+
+
+def test_get():
+    cache = Cache("mtoolbox_test")
+    cache.flush()
+
+    assert cache.get("test", lambda: 1) == 1
+    assert cache.get("test", lambda: 2) == 1
+    assert cache.get("test", lambda: 2, invalidate=True) == 2
+    assert cache.get("test", lambda: 1, maxage=timedelta(seconds=10)) == 2
+    assert cache.get("test", lambda: 1, maxage=timedelta(seconds=0)) == 1
+
+    cache.flush()
+    assert cache.get("test", lambda: 3) == 3
