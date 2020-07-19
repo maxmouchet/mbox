322e3b4525f34f65f6bcedba65dbb07de0bcc60e
Maxime Mouchet <max@maxmouchet.com>
2020-07-19 19:54:34 +0200

    Add sample_groups

---
+++ tests/test_random.py
@@ -0,0 +1,16 @@
+import pytest
+
+from mtoolbox.random import sample_groups
+
+
+def test_sample_groups():
+    population = [[None for _ in range(10)] for _ in range(20)]
+
+    groups = sample_groups(population, 0.2)
+    assert all(len(group) == 2 for group in groups)
+
+    groups = sample_groups(population, 8)
+    assert all(len(group) == 8 for group in groups)
+
+    with pytest.raises(ValueError):
+        sample_groups(population, 2.0)
