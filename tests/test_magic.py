8b148cc80a6d00a4342f86f6d88baa016eb815d6
Maxime Mouchet <max@maxmouchet.com>
2020-09-09 21:24:35 +0200

    magic: test on empty file

--- tests/test_magic.py
+++ tests/test_magic.py
@@ -2,5 +2,6 @@ from mtoolbox.magic import CompressionFormat, detect_compression
 
 
 def test_detect_compression():
+    assert detect_compression("tests/data/empty.txt") == CompressionFormat.Unknown
     assert detect_compression("tests/data/hello.txt") == CompressionFormat.Unknown
     assert detect_compression("tests/data/hello.txt.zst") == CompressionFormat.Zstandard
